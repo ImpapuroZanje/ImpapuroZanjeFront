3f52760306b6b226a077c5f67155489cf9541095
NSANZABANDI Fiacre <fiacre.nsanzabandi@soprasteria.com>
2020-02-19 17:07:01 +0100

    Added a bunch of things: Routing Technical informations Different components on the app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { TechnicalInformationComponent } from './component/technical-information/technical-information.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'technicalInformation',
+    component: TechnicalInformationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/technical-information/technical-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technical-information',
+  templateUrl: './technical-information.component.html',
+  styleUrls: ['./technical-information.component.css']
+})
+export class TechnicalInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
